07e037ed9a5a3666372c21a5b39019d3f501b78d
Drynne <bella.221145@gmail.com>
2023-11-25 22:06:25 -0300

    Deletar arquivos

--- conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = 'localhost';
-$dbname = 'bdlogin0911';
-$username = 'root';
-$password = '';
-
-$pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-
-?>
--- excluir.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('conn.php');
-
-$id = $_GET['id'];
-$stmt = $pdo->prepare('DELETE FROM tbcliente WHERE codCliente = ?');
-$stmt->execute([$id]);
-
-header('Location: acesso_completo.php');
-?>
